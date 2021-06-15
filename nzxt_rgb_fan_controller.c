a367a0be495a0cb835fc04d72d35460eed7fc0eb
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-06-15 06:03:31 +0600

    PWM, voltage, current

--- nzxt_rgb_fan_controller.c
+++ nzxt_rgb_fan_controller.c
@@ -11,35 +11,77 @@
 #include <asm/byteorder.h>
 #include <asm/unaligned.h>
 
-enum { INPUT_REPORT_ID_STATUS = 0x67 };
+/*
+ * The device has only 3 fan channels/connectors. But all HID reports have
+ * space reserved for up to 8 channels.
+ */
+#define FAN_CHANNELS 3
+#define FAN_CHANNELS_MAX 8
 
-enum {
-	STATUS_REPORT_MAGIC_FAN_STATUS = 0x02,
-	STATUS_REPORT_MAGIC_UNKNOWN_STATUS = 0x04
-};
+enum { INPUT_REPORT_ID_FAN_STATUS = 0x67 };
 
-struct status_report_header {
-	uint8_t report_id; /* INPUT_REPORT_ID_STATUS = 0x67 */
-	uint8_t magic;
-} __attribute__((__packed__));
+enum { FAN_STATUS_REPORT_SPEED = 0x02, FAN_STATUS_REPORT_VOLTAGE = 0x04 };
 
 struct fan_status_report {
+	/* report_id should be INPUT_REPORT_ID_STATUS = 0x67 */
 	uint8_t report_id;
-	uint8_t magic;
+	/* FAN_STATUS_REPORT_SPEED = 0x02 or FAN_STATUS_REPORT_VOLTAGE = 0x04 */
+	uint8_t type;
+	/* Some configuration data? Doesn't change with fan speed. Same for both 'type' values. */
 	uint8_t unknown1[22];
-	__le16 fan_rpm[3];
-	uint8_t unknown2[34];
+
+	union {
+		struct {
+			/* Fan speed, in RPM. Zero for channels without fans connected. */
+			__le16 fan_rpm[FAN_CHANNELS_MAX];
+			/* Fan duty cycle, in percent. Non-zero even for channels without fans connected. */
+			uint8_t duty_percent[FAN_CHANNELS_MAX];
+			/* Exactly the same values as duty_percent[], non-zero for disconnected fans too. */
+			uint8_t duty_percent_dup[FAN_CHANNELS_MAX];
+			/* "Case Noise" in db */
+			uint8_t noise_db;
+			uint8_t zero_padding4[7];
+		} __attribute__((__packed__)) fan_speed;
+		struct {
+			/* Voltage, in millivolts. Non-zero even when fan is not connected */
+			__le16 fan_in[FAN_CHANNELS_MAX];
+			/* Current, in milliamperes. Near-zero when disconnected */
+			__le16 fan_current[FAN_CHANNELS_MAX];
+			uint8_t zero_padding2[8];
+		} __attribute__((__packed__)) fan_voltage;
+	} __attribute__((__packed__));
+} __attribute__((__packed__));
+
+enum {
+	OUTPUT_REPORT_ID_INIT_COMMAND = 0x60,
+	OUTPUT_REPORT_ID_SET_FAN_SPEED = 0x62,
+};
+
+struct set_fan_speed_report {
+	/* report_id should be OUTPUT_REPORT_ID_SET_FAN_SPEED = 0x62 */
+	uint8_t report_id;
+	/* Should be 0x01 */
+	uint8_t magic;
+	/* To change fan speed on i-th channel, set i-th bit here */
+	uint8_t channel_bit_mask;
+	/* Fan duty cycle/target speed in percent */
+	uint8_t duty_percent[FAN_CHANNELS_MAX];
+	uint8_t zero_padding[50];
 } __attribute__((__packed__));
 
 static const uint8_t INIT_DATA[][64] = {
-	{ 0x60, 0x03 },
-	{ 0x60, 0x02, 0x01, 0xe8, 0x03, 0x01, 0xe8, 0x03 }
+	{ OUTPUT_REPORT_ID_INIT_COMMAND, 0x03 },
+	{ OUTPUT_REPORT_ID_INIT_COMMAND, 0x02, 0x01, 0xe8, 0x03, 0x01, 0xe8,
+	  0x03 }
 };
 
 struct drvdata {
 	struct hid_device *hid;
 	struct device *hwmon;
-	uint16_t fan_rpm[3];
+	uint16_t fan_rpm[FAN_CHANNELS];
+	uint8_t fan_duty_percent[FAN_CHANNELS];
+	uint16_t fan_in[FAN_CHANNELS];
+	uint16_t fan_current[FAN_CHANNELS];
 };
 
 static void handle_fan_status_report(struct drvdata *drvdata, void *data,
@@ -48,42 +90,32 @@ static void handle_fan_status_report(struct drvdata *drvdata, void *data,
 	struct fan_status_report *report = data;
 	int i;
 
-	if (size < sizeof(struct fan_status_report)) {
-		pr_warn("Fan status report size too small: %d (should be more than %zu)\n",
+	if (size != sizeof(struct fan_status_report)) {
+		pr_warn("Status report size is wrong: %d (should be %zu)\n",
 			size, sizeof(struct fan_status_report));
 		return;
 	}
 
-	if (size > sizeof(struct fan_status_report)) {
-		pr_warn("Fan status report size too large: %d (should be %zu, ignoring excess data)\n",
-			size, sizeof(struct fan_status_report));
+	switch (report->type) {
+	case FAN_STATUS_REPORT_SPEED:
+		for (i = 0; i < FAN_CHANNELS; i++) {
+			drvdata->fan_rpm[i] = get_unaligned_le16(
+				&report->fan_speed.fan_rpm[i]);
+			drvdata->fan_duty_percent[i] =
+				report->fan_speed.duty_percent[i];
+		}
 		return;
-	}
-
-	for (i = 0; i < 3; i++)
-		drvdata->fan_rpm[i] = get_unaligned_le16(&report->fan_rpm[i]);
-}
-
-static void handle_status_report(struct drvdata *drvdata, void *data, int size)
-{
-	struct status_report_header *header = data;
-
-	if (size < sizeof(struct status_report_header)) {
-		pr_warn("Status report size too small: %d (should be more than %zu)\n",
-			size, sizeof(struct status_report_header));
-		return;
-	}
-
-	switch (header->magic) {
-	case STATUS_REPORT_MAGIC_FAN_STATUS:
-		handle_fan_status_report(drvdata, data, size);
-		return;
-	case STATUS_REPORT_MAGIC_UNKNOWN_STATUS:
-		/* No idea what it means and how to parse it */
+	case FAN_STATUS_REPORT_VOLTAGE:
+		for (i = 0; i < FAN_CHANNELS; i++) {
+			drvdata->fan_in[i] = get_unaligned_le16(
+				&report->fan_voltage.fan_in[i]);
+			drvdata->fan_current[i] = get_unaligned_le16(
+				&report->fan_voltage.fan_current[i]);
+		}
 		return;
 	default:
-		pr_warn("Unexpected value of 'magic' field: %d (report size %d)\n",
-			header->magic, size);
+		pr_warn("Unexpected value of 'type' field: %d (report size %d)\n",
+			report->type, size);
 		return;
 	}
 }
@@ -91,9 +123,6 @@ static void handle_status_report(struct drvdata *drvdata, void *data, int size)
 static int hwmon_read_fan(struct drvdata *drvdata, u32 attr, int channel,
 			  long *val)
 {
-	if (channel < 0 || channel >= ARRAY_SIZE(drvdata->fan_rpm))
-		return -EINVAL;
-
 	switch (attr) {
 	case hwmon_fan_input:
 		*val = drvdata->fan_rpm[channel];
@@ -104,9 +133,51 @@ static int hwmon_read_fan(struct drvdata *drvdata, u32 attr, int channel,
 	}
 }
 
+static int hwmon_read_pwm(struct drvdata *drvdata, u32 attr, int channel,
+			  long *val)
+{
+	switch (attr) {
+	case hwmon_pwm_input:
+		*val = drvdata->fan_duty_percent[channel] * 255 / 100;
+		return 0;
+
+	default:
+		return -EINVAL;
+	}
+}
+
+static int hwmon_read_in(struct drvdata *drvdata, u32 attr, int channel,
+			 long *val)
+{
+	switch (attr) {
+	case hwmon_in_input:
+		*val = drvdata->fan_in[channel];
+		return 0;
+
+	default:
+		return -EINVAL;
+	}
+}
+
+static int hwmon_read_curr(struct drvdata *drvdata, u32 attr, int channel,
+			   long *val)
+{
+	switch (attr) {
+	case hwmon_curr_input:
+		*val = drvdata->fan_current[channel];
+		return 0;
+
+	default:
+		return -EINVAL;
+	}
+}
+
 static umode_t hwmon_is_visible(const void *data, enum hwmon_sensor_types type,
 				u32 attr, int channel)
 {
+	if (type == hwmon_pwm && attr == hwmon_pwm_input)
+		return S_IRUGO | S_IWUSR;
+
 	return S_IRUGO;
 }
 
@@ -114,16 +185,48 @@ static int hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 		      u32 attr, int channel, long *val)
 {
 	struct drvdata *drvdata = dev_get_drvdata(dev);
-	int ret;
 
 	switch (type) {
 	case hwmon_fan:
-		ret = hwmon_read_fan(drvdata, attr, channel, val);
-		break;
+		return hwmon_read_fan(drvdata, attr, channel, val);
+
+	case hwmon_pwm:
+		return hwmon_read_pwm(drvdata, attr, channel, val);
+
+	case hwmon_in:
+		return hwmon_read_in(drvdata, attr, channel, val);
+
+	case hwmon_curr:
+		return hwmon_read_curr(drvdata, attr, channel, val);
 
 	default:
-		ret = -EINVAL;
+		return -EINVAL;
 	}
+}
+
+static int hwmon_write(struct device *dev, enum hwmon_sensor_types type,
+		       u32 attr, int channel, long val)
+{
+	struct drvdata *drvdata = dev_get_drvdata(dev);
+	struct set_fan_speed_report *report;
+	int ret;
+
+	if (type != hwmon_pwm || attr != hwmon_pwm_input)
+		return -EINVAL;
+
+	report = kzalloc(sizeof(struct set_fan_speed_report), GFP_KERNEL);
+	if (!report)
+		return -ENOMEM;
+
+	report->report_id = OUTPUT_REPORT_ID_SET_FAN_SPEED;
+	report->magic = 1;
+	report->channel_bit_mask = 1 << channel;
+	report->duty_percent[channel] = val * 100 / 255;
+
+	ret = hid_hw_output_report(drvdata->hid, (void *)report,
+				   sizeof(*report));
+
+	kfree(report);
 
 	return ret;
 }
@@ -131,10 +234,15 @@ static int hwmon_read(struct device *dev, enum hwmon_sensor_types type,
 static const struct hwmon_ops hwmon_ops = {
 	.is_visible = hwmon_is_visible,
 	.read = hwmon_read,
+	.write = hwmon_write,
 };
 
 static const struct hwmon_channel_info *channel_info[] = {
 	HWMON_CHANNEL_INFO(fan, HWMON_F_INPUT, HWMON_F_INPUT, HWMON_F_INPUT),
+	HWMON_CHANNEL_INFO(pwm, HWMON_PWM_INPUT, HWMON_PWM_INPUT,
+			   HWMON_PWM_INPUT),
+	HWMON_CHANNEL_INFO(in, HWMON_I_INPUT, HWMON_I_INPUT, HWMON_I_INPUT),
+	HWMON_CHANNEL_INFO(curr, HWMON_C_INPUT, HWMON_C_INPUT, HWMON_C_INPUT),
 	NULL
 };
 
@@ -170,13 +278,13 @@ static int hid_raw_event(struct hid_device *hdev, struct hid_report *report,
 	struct drvdata *drvdata = hid_get_drvdata(hdev);
 	uint8_t report_id = *data;
 
-	if (report_id != INPUT_REPORT_ID_STATUS) {
+	if (report_id != INPUT_REPORT_ID_FAN_STATUS) {
 		pr_warn("Unknown input report: id %#x, size %d\n", report_id,
 			size);
 		return 0;
 	}
 
-	handle_status_report(drvdata, data, size);
+	handle_fan_status_report(drvdata, data, size);
 	return 0;
 }
 
@@ -238,10 +346,9 @@ static void hid_remove(struct hid_device *hdev)
 	hid_hw_stop(hdev);
 }
 
-static const struct hid_device_id hid_id_table[] = {
-	{ HID_USB_DEVICE(0x1e71, 0x2009) },
-	{}
-};
+static const struct hid_device_id hid_id_table[] = { { HID_USB_DEVICE(0x1e71,
+								      0x2009) },
+						     {} };
 
 static struct hid_driver hid_driver = {
 	.name = "nzxt_rgb_fan_controller",
@@ -254,12 +361,12 @@ static struct hid_driver hid_driver = {
 #endif
 };
 
-static int __init nzxtfan_init(void)
+static int __init nzxt_rgb_fan_controller_init(void)
 {
 	return hid_register_driver(&hid_driver);
 }
 
-static void __exit nzxtfan_exit(void)
+static void __exit nzxt_rgb_fan_controller_exit(void)
 {
 	hid_unregister_driver(&hid_driver);
 }
@@ -269,5 +376,5 @@ MODULE_AUTHOR("Aleksandr Mezin <mezin.alexander@gmail.com>");
 MODULE_DESCRIPTION("Driver for NZXT RGB & Fan controller");
 MODULE_LICENSE("GPL");
 
-late_initcall(nzxtfan_init);
-module_exit(nzxtfan_exit);
+late_initcall(nzxt_rgb_fan_controller_init);
+module_exit(nzxt_rgb_fan_controller_exit);
